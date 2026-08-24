022dbcf3fda6f7f92b4d431457c32b1f82c0d56b
Paul Nercessian <122783688+Bouwles@users.noreply.github.com>
2026-08-24 09:50:22 +0400

    build: clarify release script output

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 @echo off
 cd /d "%~dp0"
-echo Building Windows installer...
+echo Building Windows installer and portable EXE...
 npm run build
 echo.
-echo Installer is in the release\ folder.
+echo Build complete.
+echo Installer and portable EXE are in the release\ folder.
 pause
